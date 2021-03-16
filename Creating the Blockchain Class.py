f583fb1af58854addb6b27a96ff84c893fdad661
raheema12 <61323745+raheema12@users.noreply.github.com>
2021-03-16 08:05:58 +0000

    Add files via upload

---
+++ Creating the Blockchain Class.py
@@ -0,0 +1,15 @@
+#imports the Block class from block.py
+from block import Block
+
+class Blockchain:
+    def __init__(self):
+      self.chain= []
+      self.all_transactions = []
+      self.genesis_block()
+      
+    
+    def genesis_block(self):
+      transactions = []
+      previous_hash ="0"
+      self.chain.append(Block(transactions,previous_hash))
+      
